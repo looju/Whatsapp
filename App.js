759da886b6063de32eb58885d43c647078fe8f79
looju <omofade2020@gmail.com>
2023-03-07 20:43:55 +0000

    subscribing to user

--- App.js
+++ App.js
@@ -2,8 +2,24 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { useAssets } from "expo-asset";
 import { ActivityIndicator } from "react-native";
+import React, { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./Config/Firebase";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      if (user) {
+        setCurrentUser(user);
+      }
+    });
+    return () => unSubscribe();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
