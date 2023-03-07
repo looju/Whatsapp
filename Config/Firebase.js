87be995c3f38ca1b9fa774e62f808bd17a342ad5
looju <omofade2020@gmail.com>
2023-03-07 18:06:14 +0000

    email functions

--- Config/Firebase.js
+++ Config/Firebase.js
@@ -1,5 +1,12 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA2kLJzfkhc7Y2q3BAA2SB_xWm8Bg5zUvE",
@@ -7,9 +14,22 @@ const firebaseConfig = {
   projectId: "whatsapp-bdd11",
   storageBucket: "whatsapp-bdd11.appspot.com",
   messagingSenderId: "620086478424",
-  appId: "1:620086478424:web:1dd38149aa32085593ac2d"
+  appId: "1:620086478424:web:1dd38149aa32085593ac2d",
 };
 
-
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
+
+export function SignIn(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function SignUp(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export function SignOut() {
+  return signOut(auth);
+}
