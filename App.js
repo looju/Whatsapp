ced03ec023cd92f375d19d203a9378c07c8a6991
looju <omofade2020@gmail.com>
2023-03-07 18:47:41 +0000

    Assets and preload

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useAssets } from "expo-asset";
+import { ActivityIndicator } from "react-native";
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +15,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+function Main() {
+  consst[Assets] = useAssets(require("./assets/favicon.png"));
+  if (!Assets) {
+    return <ActivityIndicator color="green" />;
+  } else {
+    return <App />;
+  }
+}
+
+export default Main;
