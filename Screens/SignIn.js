c835903d4564230e97fd6c6e60960e9d32fcd253
looju <omofade2020@gmail.com>
2023-03-08 13:30:44 +0000

    Await mode

--- Screens/SignIn.js
+++ Screens/SignIn.js
@@ -1,5 +1,6 @@
 import { View, Text, StyleSheet, Image, TextInput } from "react-native";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { SignUp, SignIn } from "../Config/firebase";
 import { Button } from "react-native-paper";
 import { GlobalContext } from "../Services/Context/Context";
 
@@ -7,6 +8,19 @@ export const SignIn = () => {
   const {
     theme: { colors },
   } = useContext(GlobalContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [mode, setMode] = useState("SignUp");
+
+  const handlePress = async () => {
+    if (mode === "SignUp") {
+      await SignUp(email, password);
+    }
+    if (mode === "SignIn") {
+      await SignIn(email, password);
+    }
+  };
 
   return (
     <View style={[Styles.container, { backgroundColor: colors.black }]}>
@@ -20,6 +34,10 @@ export const SignIn = () => {
       />
       <View style={Styles.inputView}>
         <TextInput
+          value={email}
+          onChangeText={(text) => {
+            setEmail(text);
+          }}
           style={[
             Styles.inputemail,
             { borderBottomColor: colors.primary, color: colors.white },
@@ -31,6 +49,10 @@ export const SignIn = () => {
           keyboardType="email-address"
         />
         <TextInput
+          value={password}
+          onChangeText={(text) => {
+            setPassword(text);
+          }}
           style={[
             Styles.inputpassword,
             { borderBottomColor: colors.primary, color: colors.white },
@@ -51,6 +73,7 @@ export const SignIn = () => {
           mode="contained"
           buttonColor={colors.secondary}
           textColor={colors.white}
+          style={{ width: 200 }}
         >
           SIGN UP
         </Button>
@@ -90,3 +113,5 @@ const Styles = StyleSheet.create({
     marginTop: 20,
   },
 });
+
+//Hello. My name is Loju. I am a mobile developer with react native. I saw the recent posting from Kuda for this position but i know i do not have the required experience.So far, i have been able to build applications that can scale such as a tinder messaging app, delivery app, news application and i am currently building something similar to WhatsApp. Please i would love if i can work as an intern or a junior  for FREE because right now i seek real world experience and not finance. I would proove my work abilities and i promise to be dutiful and give my best to Kuda bank. Attached here is my CV sir. Thank you sir
