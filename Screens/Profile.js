51b4632c8da68d81b73a376a856e3099824e7923
looju <omofade2020@gmail.com>
2023-03-09 20:40:42 +0000

    Profile screen

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,11 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import Constants from "expo-constants";
+import { GlobalContext } from "../Services/Context/Context";
 
 export const Profile = () => {
-  return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
-}
+  const {
+    theme: { colors },
+  } = useContext(GlobalContext);
 
+  return (
+    <View
+      style={[
+        Styles.container,
+        {
+          backgroundColor: colors.black,
+          paddingTop: Constants.statusBarHeight+20,
+          padding:20
+        },
+      ]}
+    >
+      <Text style={{color:colors.white}}>Profile</Text>
+    </View>
+  );
+};
+
+const Styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+});
