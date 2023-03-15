0d032acd8c2a24a9cda28c7bc0eb37c31541e706
looju <omofade2020@gmail.com>
2023-03-15 12:50:52 +0000

    Query rooms

--- Screens/Home/Chat.js
+++ Screens/Home/Chat.js
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, {useContext} from "react";
+import { db, auth } from "../../Config/Firebase";
+import { query, collection, where } from "firebase/firestore";
+import { GlobalContext } from './../../Services/Context/Context';
 
 export const Chat = () => {
+  const { currentUser } = auth;
+  const {rooms, setRooms}=useContext(GlobalContext)
+
+  const chatsQuery = query(
+    collection(db, "rooms"),
+    where("participantsarray", "array-contains", currentUser.email)
+  );
   return (
     <View>
       <Text>Chat</Text>
     </View>
-  )
-}
-
+  );
+};
