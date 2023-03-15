f5a9803deb62d2a4b34fe5d3268093e2e39023cc
looju <omofade2020@gmail.com>
2023-03-15 14:01:10 +0000

    Parse chats

--- Screens/Home/Chat.js
+++ Screens/Home/Chat.js
@@ -1,17 +1,32 @@
 import { View, Text } from "react-native";
-import React, {useContext} from "react";
+import React, { useContext, useEffect } from "react";
 import { db, auth } from "../../Config/Firebase";
-import { query, collection, where } from "firebase/firestore";
-import { GlobalContext } from './../../Services/Context/Context';
+import { query, collection, where, onSnapshot, doc } from "firebase/firestore";
+import { GlobalContext } from "./../../Services/Context/Context";
 
 export const Chat = () => {
   const { currentUser } = auth;
-  const {rooms, setRooms}=useContext(GlobalContext)
+  const { rooms, setRooms } = useContext(GlobalContext);
 
   const chatsQuery = query(
     collection(db, "rooms"),
     where("participantsarray", "array-contains", currentUser.email)
   );
+
+  useEffect(() => {
+    const unSubscribe = onSnapshot(chatsQuery, (querySnapshot) => {
+      const parsedChats = querySnapshot.docs
+        .filter((doc) => doc.data().lastMessage) // to create an array containing documents with the last message property
+        .map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+          userB: doc.data.participants.find(
+            (p) => p.email !== currentUser.email
+          ), // So that user B is the other user possessing a different email
+        }));
+    });
+  }, []);
+
   return (
     <View>
       <Text>Chat</Text>
