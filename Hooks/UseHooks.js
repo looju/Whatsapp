40e272160c18f49bb14b879f31864cd13c6f76dd
looju <omofade2020@gmail.com>
2023-04-18 10:45:13 +0100

    contact status

--- Hooks/UseHooks.js
+++ Hooks/UseHooks.js
@@ -1,2 +1,20 @@
-import {useEffect,useState} from "react"
-import * as Contacts from 'expo-contacts';
+import { useEffect, useState } from "react";
+import * as Contacts from "expo-contacts";
+import { ModalComponent } from "./../Components/General/Modal";
+
+export const UseContacts = () => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status = "denied") {
+        <ModalComponent message={"Failed permission"} buttonMessage={"OK!"} />;
+      }
+      if(status="granted"){
+        
+      }
+
+    })();
+  }, []);
+};
