eca3e9a8655407bccc4edbeaae8f4a654881feea
looju <omofade2020@gmail.com>
2023-04-18 13:11:58 +0100

    props

--- Components/General/ListItem.js
+++ Components/General/ListItem.js
@@ -1,16 +1,32 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { GlobalContext } from "./../../Services/Context/Context";
 
-export const ListItem = ({style}) => {
+export const ListItem = ({ type, description, user, room, image, style }) => {
+  const navigation = useNavigation();
+  const {
+    theme: { colors },
+  } = useContext(GlobalContext);
+
   return (
-    <TouchableOpacity style={[Styles.container,{...style}]}>
+    <TouchableOpacity
+      style={[Styles.container, { ...style }]}
+      onPress={() =>
+        navigation.navigate("chat", {
+          user: user,
+          room: room,
+          image: image,
+        })
+      }
+    >
       <Text>ListItem</Text>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-const Styles=StyleSheet.create({
-    container:{
-        height:80
-    }
-})
+const Styles = StyleSheet.create({
+  container: {
+    height: 80,
+  },
+});
