ad86bd755359969f99994d6ca3f7e358a70cab28
looju <omofade2020@gmail.com>
2023-04-18 10:59:41 +0100

    map contatcs

--- Hooks/UseHooks.js
+++ Hooks/UseHooks.js
@@ -5,16 +5,39 @@ import { ModalComponent } from "./../Components/General/Modal";
 export const UseContacts = () => {
   const [contacts, setContacts] = useState([]);
 
+  const mapContactToUser = (contact) => {
+    return {
+      contactName:
+        contact.firstName && contact.lastName
+          ? `${contact.firstName} ${contact.lastName}`
+          : contact.firstName
+    };
+  };
+
   useEffect(() => {
     (async () => {
       const { status } = await Contacts.requestPermissionsAsync();
-      if (status = "denied") {
+      if ((status = "denied")) {
         <ModalComponent message={"Failed permission"} buttonMessage={"OK!"} />;
       }
-      if(status="granted"){
-        
+      if ((status = "granted")) {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Emails],
+        });
+        if (data.length > 0) {
+          setContacts(
+            data
+              .filter(
+                (value) =>
+                  value.firstName &&
+                  value.emails &&
+                  value.emails[0] &&
+                  value.emails[0].email
+              )
+              .map(mapContactToUser)
+          );
+        }
       }
-
     })();
   }, []);
 };
