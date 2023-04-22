48881727230143c09b1634a3bbac9891c298478f
looju <omofade2020@gmail.com>
2023-04-22 12:30:27 +0100

    const

--- Screens/ChatRoom/ChatRoom.js
+++ Screens/ChatRoom/ChatRoom.js
@@ -1,7 +1,14 @@
+//@refresh reset
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useRoute } from '@react-navigation/native';
+import { auth } from '../../Config/Firebase';
 
 export const ChatRoom = () => {
+  const route=useRoute()
+  const {currentUser}=auth
+  const room=route.params.room
+  const selectedImage=route.params.image
   return (
     <View>
       <Text>ChatRoom</Text>
