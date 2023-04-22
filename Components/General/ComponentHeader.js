f98e9cc00a0f93041c6886dff1716e8e6d88d12e
looju <omofade2020@gmail.com>
2023-04-22 09:11:56 +0100

    ComponentHeader

---
+++ Components/General/ComponentHeader.js
@@ -0,0 +1,63 @@
+import React, { useContext } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+import { Avatar } from "./Avatar";
+import { useRoute } from "@react-navigation/native";
+import { GlobalContext } from "../../Services/Context/Context";
+
+export const ComponentHeader = ({ ind, routeVal, avatar, title }) => {
+  const route = useRoute();
+  const {
+    theme: { colors },
+  } = useContext(GlobalContext);
+  return (
+    <View style={Styles.container}>
+      {avatar && (
+        <View>
+          <Avatar size={40} user={route.params.user} />
+        </View>
+      )}
+
+      {routeVal && (
+        <View style={Styles.title}>
+          <Text style={{ color: colors.white, fontSize: 18 }}>
+            {route.params.user.contactName || route.params.user.displayName}
+          </Text>
+        </View>
+      )}
+
+      {title && (
+        <View style={Styles.title}>
+          <Text style={{ color: colors.white, fontSize: 18 }}>{title}</Text>
+        </View>
+      )}
+
+      {ind == "true" && (
+        <View style={Styles.ind}>
+          <ActivityIndicator color={colors.white} size={17} />
+        </View>
+      )}
+    </View>
+  );
+};
+
+const Styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  title: {
+    marginLeft: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  ind: {
+    left: Dimensions.get("screen").width * 0.4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
