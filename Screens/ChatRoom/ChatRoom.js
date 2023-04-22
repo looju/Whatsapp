040b6a9a0256b49aa138394ac6945b075096b31e
looju <omofade2020@gmail.com>
2023-04-22 12:42:02 +0100

    Params

--- Screens/ChatRoom/ChatRoom.js
+++ Screens/ChatRoom/ChatRoom.js
@@ -9,6 +9,9 @@ export const ChatRoom = () => {
   const {currentUser}=auth
   const room=route.params.room
   const selectedImage=route.params.image
+  const userB=route.params.user
+
+  const senderUser=currentUser.photoURL?{name:currentUser.displayName, _id:currentUser.uid, avatar:currentUser.photoURL}
   return (
     <View>
       <Text>ChatRoom</Text>
