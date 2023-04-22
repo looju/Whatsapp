f54e08f76ce773aeb30d25950df6b31fa567e378
looju <omofade2020@gmail.com>
2023-04-22 12:45:47 +0100

    randomId

--- Screens/ChatRoom/ChatRoom.js
+++ Screens/ChatRoom/ChatRoom.js
@@ -1,21 +1,33 @@
 //@refresh reset
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useRoute } from '@react-navigation/native';
-import { auth } from '../../Config/Firebase';
+import { View, Text } from "react-native";
+import React from "react";
+import { useRoute } from "@react-navigation/native";
+import { auth } from "../../Config/Firebase";
+import "react-native-get-random-values"
+import { nanoid } from 'nanoid';
 
 export const ChatRoom = () => {
-  const route=useRoute()
-  const {currentUser}=auth
-  const room=route.params.room
-  const selectedImage=route.params.image
-  const userB=route.params.user
+  const route = useRoute();
+  const { currentUser } = auth;
+  const room = route.params.room;
+  const selectedImage = route.params.image;
+  const userB = route.params.user;
 
-  const senderUser=currentUser.photoURL?{name:currentUser.displayName, _id:currentUser.uid, avatar:currentUser.photoURL}
+  const randomId=nanoid()
+
+  const senderUser = currentUser.photoURL
+    ? {
+        name: currentUser.displayName,
+        _id: currentUser.uid,
+        avatar: currentUser.photoURL,
+      }
+    : { name: currentUser.displayName, _id: currentUser.uid };
+
+
+  const roomId=room?room.id : randomId  
   return (
     <View>
       <Text>ChatRoom</Text>
     </View>
-  )
-}
-
+  );
+};
