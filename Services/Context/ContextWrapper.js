1e55c54f1c12653c94c16f9086539188509f8c0c
looju <omofade2020@gmail.com>
2023-04-24 14:14:46 +0100

    context

--- Services/Context/ContextWrapper.js
+++ Services/Context/ContextWrapper.js
@@ -1,14 +1,15 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import { GlobalContext } from "./Context";
 import { theme } from "../../Utilities/Utilities";
 
 export const ContextWrapper = (props) => {
-
-const [rooms, setRooms]=useState([])
+  const [rooms, setRooms] = useState([]);
 
   return (
-    <GlobalContext.Provider value={{ theme, rooms,setRooms }}>
+    <GlobalContext.Provider
+      value={{ theme, rooms, setRooms, unfilteredRooms, setUnfilteredRooms }}
+    >
       {props.children}
     </GlobalContext.Provider>
   );
-}
+};
