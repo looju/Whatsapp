ae071100579e784f698837dcb972331b53382695
looju <omofade2020@gmail.com>
2023-04-26 15:19:02 +0100

    State

--- Screens/Home/Photo.js
+++ Screens/Home/Photo.js
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React,{useState} from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export const Photo = () => {
+  const navigation = useNavigation();
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(()=>{
+
+  },[navigation, cancelled])
+
   return (
     <View>
       <Text>Photo</Text>
     </View>
-  )
-}
-
+  );
+};
